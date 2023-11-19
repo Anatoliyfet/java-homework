d48e60f460790af8f1941599800c443f1b943042
fet anatoliy <anatoliyfet@gmail.com>
2023-11-19 19:02:42 +0300

    add homework module05 first commit

--- module05/src/main/java/ru/sberbank/edu/CityInfo.java
+++ module05/src/main/java/ru/sberbank/edu/CityInfo.java
@@ -1,5 +1,7 @@
 package ru.sberbank.edu;
 
+import java.util.Objects;
+
 /**
  * City info
  */
@@ -7,6 +9,7 @@ public class CityInfo {
 
     private String name;
     private GeoPosition position;
+
 
     /**
      * Ctor.
@@ -16,6 +19,58 @@ public class CityInfo {
      */
     public CityInfo(String name, GeoPosition position) {
         this.name = name;
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CityInfo cityInfo = (CityInfo) o;
+        return name.equals(cityInfo.name.toUpperCase());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name.toUpperCase());
+    }
+
+    /**
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return
+     */
+    public GeoPosition getPosition() {
+        return position;
+    }
+
+    /**
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "CityInfo{" +
+                "name='" + name + '\'' +
+                ", position=" + position +
+                '}';
+    }
+
+    /**
+     * @param position
+     */
+    public void setPosition(GeoPosition position) {
         this.position = position;
     }
 }
